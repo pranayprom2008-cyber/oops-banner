69d92e82bbbb86b401110e13ee5dffe05d1a46c0
pranayprom2008-cyber <pranayprom2008@gmail.com>
2026-02-17 09:45:10 +0530

    Fix OSBanner class and correct output statements
    Fix class and method names, and correct print statements.

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+class OSBanner { 
+    public static void  main(string[] args) {
 
+        system.out.printin("*****   ***** ");
+        system.out.printin("*   *   *    *");
+        system.out.printin("*   *   *    *");
+        system.out.printin("*   *     ****");
+        system.out.printin("*   *   *    * ");
+        system.out.printin("*   *   *    * ");
+        system.out.printin("*****   ***** ");
+    }
+}
